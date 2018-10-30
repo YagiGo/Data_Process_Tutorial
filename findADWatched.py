68103b7097cbdd43c5657109770e04dd60fdee64
Zhaoxin Wu <jeremywu1995@gmail.com>
2018-10-30 14:48:59 +0900

    把一个大数据分成数个小数据，方便进行并行处理，增加try except

--- findADWatched.py
+++ findADWatched.py
@@ -19,50 +19,51 @@ def matchingCMAndUser(collectionName):
     tv_watch_data_collection_size = tv_watch_data_collection.count()
     index = 0
     # print(tv_watch_data_collection)
-    for single_tv_watch_data in tv_watch_data_collection.find():
-        print("正在对第{}条电视观看数据进行匹配，共有{}条数据".format(index, tv_watch_data_collection_size))
-        start_timestamp = single_tv_watch_data["start_timestamp"]
-        end_timestamp = single_tv_watch_data["end_timestamp"]
-        for single_cm_data in cm_data_collection.find():
-            if start_timestamp <= single_cm_data["timestamp"] <= end_timestamp and single_tv_watch_data["TV_station_code"] == single_cm_data["TV_station_code"]:
-                # print("Process No.{} FIND MATCH".format(multiprocessing.current_process()))
-                cm_user_watch_document = {
-                    "user_watch_date": single_tv_watch_data["date"],
-                    "user_watch_data_SEQ": single_tv_watch_data["data_SEQ"],
-                    "user_watch_day_of_week": single_tv_watch_data["day of week"],
-                    "user_watch_personal_num": single_tv_watch_data["personal_num"],
-                    "user_watch_household_num": single_tv_watch_data["household_num"],
-                    "user_watch_TVNo": single_tv_watch_data["TVNo"],
-                    "user_watch_TV_station_code": single_tv_watch_data["TV_station_code"],
-                    "user_watch_data_category": single_tv_watch_data["data_category"],
-                    "user_watch_end_timestamp": single_tv_watch_data["end_timestamp"],
-                    "user_watch_start_timestamp": single_tv_watch_data["start_timestamp"],
-                    "user_watch_last_time": single_tv_watch_data["last_time"],
-                    "cm_brand_code": single_cm_data["brand_code"],
-                    "advertiser_code": single_cm_data["advertiser_code"],
-                    "cm_type": single_cm_data["CM_type"],
-                    "cm_last_time": single_cm_data["last_time"],
-                    "cm_tv_station_code": single_cm_data["TV_station_code"],
-                    "cm_timestamp": single_cm_data["timestamp"]
-                }
-                print(cm_user_watch_document)
-                cm_user_match_collection.insert_one(cm_user_watch_document)
-                # print("插入成功")
+    try:
+        for single_tv_watch_data in tv_watch_data_collection.find():
+            index += 1
+            print("{}进程正在对第{}条电视观看数据进行匹配，共有{}条数据".format(multiprocessing.current_process(), index, tv_watch_data_collection_size))
+            start_timestamp = single_tv_watch_data["start_timestamp"]
+            end_timestamp = single_tv_watch_data["end_timestamp"]
+            for single_cm_data in cm_data_collection.find():
+                if start_timestamp <= single_cm_data["timestamp"] <= end_timestamp and single_tv_watch_data["TV_station_code"] == single_cm_data["TV_station_code"]:
+                    # print("Process No.{} FIND MATCH".format(multiprocessing.current_process()))
+                    cm_user_watch_document = {
+                        "user_watch_date": single_tv_watch_data["date"],
+                        "user_watch_data_SEQ": single_tv_watch_data["data_SEQ"],
+                        "user_watch_day_of_week": single_tv_watch_data["day of week"],
+                        "user_watch_personal_num": single_tv_watch_data["personal_num"],
+                        "user_watch_household_num": single_tv_watch_data["household_num"],
+                        "user_watch_TVNo": single_tv_watch_data["TVNo"],
+                        "user_watch_TV_station_code": single_tv_watch_data["TV_station_code"],
+                        "user_watch_data_category": single_tv_watch_data["data_category"],
+                        "user_watch_end_timestamp": single_tv_watch_data["end_timestamp"],
+                        "user_watch_start_timestamp": single_tv_watch_data["start_timestamp"],
+                        "user_watch_last_time": single_tv_watch_data["last_time"],
+                        "cm_brand_code": single_cm_data["brand_code"],
+                        "advertiser_code": single_cm_data["advertiser_code"],
+                        "cm_type": single_cm_data["CM_type"],
+                        "cm_last_time": single_cm_data["last_time"],
+                        "cm_tv_station_code": single_cm_data["TV_station_code"],
+                        "cm_timestamp": single_cm_data["timestamp"]
+                    }
+                    print(cm_user_watch_document)
+                    cm_user_match_collection.insert_one(cm_user_watch_document)
+                    # print("插入成功")
+    except:
+        print("插入出错，无视此条记录")
 
 #  把一个任务分成八部分，分给八个核
-client = MongoClient("localhost", 27017)
-tv_watch_data_collection = client["all-tv-orgn-data"]["raw_data"]
-tv_watch_data_collection_size = tv_watch_data_collection.count()
-chunck_size = int(tv_watch_data_collection_size / 4)
 if __name__ == "__main__":
     pool = multiprocessing.Pool(processes=4)
     sub_collections = ["sub_collection_00", "sub_collection_01", "sub_collection_02", "sub_collection_03"]
-    pool.apply_async(matchingCMAndUser, args=(collection_name for collection_name in sub_collections))
-    pool.close()
-    pool.join()
-
-    # matchingCMAndUser(0, chunck_size)
-
+    """
+    for collection_name in sub_collections:
+        pool.apply_async(matchingCMAndUser, args=(collection_name,))
+    """
+    # pool.close()
+    # pool.join()
+    matchingCMAndUser(sub_collections[3])
 
 
 
